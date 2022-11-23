e46c1f1659ae18879ec661f1de3c3486df59a7c9
Christian Biasuzzi <christian.biasuzzi@soft.it>
2022-11-23 14:03:07 +0100

    fixes
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- pypowsybl_jupyter/explorersld.py
+++ pypowsybl_jupyter/explorersld.py
@@ -5,17 +5,18 @@
 
 from IPython.display import display
 from ipywidgets import widgets
-from pypowsybl.network import Network
+from pypowsybl.network import Network, LayoutParameters
 from pypowsybl_jupyter import display_sld_svg
 
 
-def network_explorer_sld(network: Network, vl_id: str = None):
+def network_explorer_sld(network: Network, vl_id: str = None, parameters: LayoutParameters = None):
     """
     created a basic, in-place, SLD network explorer
 
     Args:
         network:  the input network
         vl_id: the starting VL to display. If None, display the first VL from network.get_voltage_levels()
+        parameters: layout properties to adjust the svg rendering for the sld
 
     Examples:
 
@@ -26,14 +27,15 @@ def network_explorer_sld(network: Network, vl_id: str = None):
 
     diagram_panel = widgets.Output()
 
-    def _show_svg_sld(_network: Network, _vl_id: str):
+    def _show_svg_sld(_network: Network, _vl_id: str, _parameters: LayoutParameters):
         with diagram_panel:
             diagram_panel.clear_output()
-            next_widget = display_sld_svg(_network.get_single_line_diagram(_vl_id, use_name=True), enable_callbacks=True)
-            next_widget.on_nextvl(lambda event: _show_svg_sld(_network, str(event.clicked_nextvl)))
+            next_widget = display_sld_svg(_network.get_single_line_diagram(_vl_id, _parameters), enable_callbacks=True)
+            next_widget.on_nextvl(lambda event: _show_svg_sld(_network, str(event.clicked_nextvl), _parameters))
             display(next_widget)
 
+    p=parameters if parameters is not None else LayoutParameters(use_name=True)
     starting_vl_id = vl_id if vl_id is not None else network.get_voltage_levels().index[0]
-    _show_svg_sld(network, starting_vl_id)
+    _show_svg_sld(network, starting_vl_id, p)
     return widgets.HBox([diagram_panel])
 
