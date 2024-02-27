c3a444d7675548dfcd6cd5809b4c1aa1fd851ef3
Christian Biasuzzi <christian.biasuzzi@soft.it>
2024-02-27 19:06:32 +0100

    adds to the SLD explorer the ability to toggle a switch.
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- pypowsybl_jupyter/explorersld.py
+++ pypowsybl_jupyter/explorersld.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, RTE (http://www.rte-france.com)
+# Copyright (c) 2022-2024, RTE (http://www.rte-france.com)
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -28,11 +28,16 @@ def network_explorer_sld(network: Network, vl_id: str = None, parameters: Layout
     diagram_panel = widgets.Output()
     diagram_panel.layout.border='2px solid lightgrey'
 
+    def _toggle_switch(_switch_id:str, _new_switch_status: bool, _network: Network, _vl_id:str):
+        network.update_switches(id=_switch_id, open=_new_switch_status)
+        _show_svg_sld(_network, _vl_id, p)
+
     def _show_svg_sld(_network: Network, _vl_id: str, _parameters: LayoutParameters):
         with diagram_panel:
             diagram_panel.clear_output()
             next_widget = display_sld_svg(_network.get_single_line_diagram(_vl_id, _parameters), enable_callbacks=True)
             next_widget.on_nextvl(lambda event: _show_svg_sld(_network, str(event.clicked_nextvl), _parameters))
+            next_widget.on_switch(lambda event: _toggle_switch(event.clicked_switch.get('id'), event.clicked_switch.get('switch_status'), _network, _vl_id))
             display(next_widget)
 
     p=parameters if parameters is not None else LayoutParameters(use_name=True)
