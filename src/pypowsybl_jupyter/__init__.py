70640756c804fd0b2df489e80a404530a144c1d5
Christian Biasuzzi <christian.biasuzzi@soft.it>
2024-06-16 18:08:59 +0200

    Removes pypowsybl from the project dependencies (but for the build dependencies). Performs a check in the __init__ to see if pypowsybl is installed and if it meets the minimum version
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- src/pypowsybl_jupyter/__init__.py
+++ src/pypowsybl_jupyter/__init__.py
@@ -7,6 +7,32 @@
 
 import importlib.metadata
 
+class DependencyError(Exception):
+    """Custom exception for dependency errors."""
+    pass
+
+from importlib.metadata import version, PackageNotFoundError
+def check_pypowsybl_version(min_version):
+    try:
+        import inspect
+        import pypowsybl
+
+        module = inspect.getmodule(pypowsybl.print_version)
+        if module is None:
+            raise PackageNotFoundError(f"Module for function {func.__name__} not found.")
+        module_name = module.__name__
+        installed_version = version(module_name)
+        if installed_version >= min_version:
+            print(f"{module_name} version {installed_version} is already installed and meets the minimum pypowsybl_jupyter requirement for pypowsybl_jupyter of {min_version}.")
+        else:
+            raise DependencyError(f"{module_name} version {installed_version} is installed but does not meet the minimum requirement for pypowsybl_jupyter of {min_version}. Please update it.")
+
+    except (PackageNotFoundError, ModuleNotFoundError) as e:
+        raise DependencyError(f"pypowsybl is not installed or could not be determined. Please install it (minimum version required for pypowsybl_jupyter is {min_version}).")
+
+#note that the real minimum version is 1.2.0. The 1.3.0 used here is for testing this approach
+check_pypowsybl_version("1.3.0")
+
 from .sldwidget import (
     SldWidget, display_sld, update_sld
 )
