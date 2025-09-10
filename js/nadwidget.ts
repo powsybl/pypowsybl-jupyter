f75488fb4831a22ea4fb47674360864284198cbe
Florian Dupuy <florian.dupuy@rte-france.com>
2025-09-11 01:00:56 +0200

    Update notebooks
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- js/nadwidget.ts
+++ js/nadwidget.ts
@@ -218,7 +218,7 @@ function render({ model, el, experimental }: RenderProps<NadWidgetModel>) {
             handleMoveTextNode,
             handleSelectNode,
             is_drag_enabled,
-            false,
+            true,
             null,
             is_hover_enabled ? handleInfo : null,
             handleMenu,
