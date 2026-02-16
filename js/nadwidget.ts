9e9084b63e56597fffc6a2151f04b7c8698319e0
Nicolas Rol <nicolas.rol@rte-france.com>
2026-02-16 15:01:41 +0100

    Rename files + fix
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- js/nadwidget.ts
+++ js/nadwidget.ts
@@ -176,7 +176,7 @@ function render({ model, el, experimental }: RenderProps<NadWidgetModel>) {
             onSelectNodeCallback: handleSelectNode,
             onToggleHoverCallback: is_hover_enabled ? handleInfo : null,
             onRightClickCallback: handleMenu,
-            enableAdaptiveTextZoom: true
+            enableAdaptiveTextZoom: true,
         };
 
         nad_viewer = new NetworkAreaDiagramViewer(
